4b172db460720aa61554350f72ec393803dd9bc2
Gustavo <gustavominatto23@gmail.com>
2023-11-21 15:45:16 -0300

    commit

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Details } from '../pages/Details';
 import { Profile } from '../pages/Profile';
 import { Home } from '../pages/Home';
 import { New } from '../pages/New';
 
-export function AppRoutes(){
-  return(
+export function AppRoutes() {
+  return (
     <Routes>
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/details/:id" element={<Details/>} />
-      <Route path="/new" element={<New/>} />
-      <Route path="/" element={<Home/>} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
